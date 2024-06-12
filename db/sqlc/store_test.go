d4be6cd9c24d063e16f52e7a48bf8c0b23a1cde1
Rodrigo <rodrigo.ortega@asapcred.com.br>
2024-06-12 15:11:37 -0300

    feat: store test created

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,66 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	// run n concurrent transfer transactions
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			results <- result
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		assert.NotEmpty(t, result)
+
+		// check transfer
+		transfer := result.Transfer
+		assert.NotEmpty(t, transfer)
+		assert.Equal(t, account1.ID, transfer.FromAccountID)
+		assert.Equal(t, account2.ID, transfer.ToAccountID)
+		assert.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		// check entries
+		fromEntry := result.FromEntry
+		assert.NotEmpty(t, fromEntry)
+		assert.Equal(t, account1.ID, fromEntry.AccountID)
+		assert.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+	}
+
+}
